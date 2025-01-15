f04b766e62dafbcc866115cd5b95fe20ed7df7d4
Hyochan <hyochan02@gmail.com>
2025-01-16 05:16:43 +0900

    design) 페이지 기본 구조 작성

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import LoginBox from "../components/LoginBox";
+
+const LoginPage = () => {
+  return (
+    <LoginPageWrapper>
+      <LoginBox />
+    </LoginPageWrapper>
+  );
+};
+
+const LoginPageWrapper = styled.div``;
+
+export default LoginPage;
