5d7b20c67c7d6fb2d30111f0b26da172587094e2
woohyun23 <parkwh0823@gmail.com>
2025-01-16 10:16:53 +0900

    design) MyPage - Note

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
-import UserPageEx from "./pages/UserPageEx";
+import UserPageEx from "./pages/UserPageNote.jsx";
 
 function App() {
   return (
