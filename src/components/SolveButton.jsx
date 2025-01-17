1b8a76261748fc7acc5b0f472b039bf2c5e7611e
woohyun23 <parkwh0823@gmail.com>
2025-01-18 00:47:47 +0900

    fix) SolveButton 수정

--- src/components/SolveButton.jsx
+++ src/components/SolveButton.jsx
@@ -1,24 +1,51 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const SolveButton = ({ children, onClick }) => {
   return <StyledButton onClick={onClick}>{children}</StyledButton>;
 };
 
 const StyledButton = styled.button`
-  width: 200px;
-  height: 80px;
-  background-color: #5887f4;
-  color: white;
-  border: none;
-  border-radius: 10px;
-  font-size: 20px;
+  width: 555px;
+  height: 85px;
+  font-size: 24px;
+  line-height: 30px;
+  border-radius: 4px;
   cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #4070d6;
+  transition: all 0.3s ease;
+  box-sizing: border-box;
+  font-family: "HakgyoansimAllimjangTTF-R";
+  /* 기본 스타일 */
+  ${({ variant }) =>
+    variant === "filled" &&
+    css`
+      background-color: #5887f4; /* 기본 배경색 */
+      color: white; /* 기본 글씨색 */
+      border: none;
+      &:hover {
+        background-color: white; /* 호버 시 배경색 */
+        color: #5887f4; /* 호버 시 글씨색 */
+        border: 2px solid #5887f4; /* 호버 시 테두리 */
+      }
+    `}
+  ${({ variant }) =>
+    variant === "outlined" &&
+    css`
+      background-color: transparent;
+      color: #5887f4;
+      border: 2px solid #5887f4;
+      &:hover {
+        background-color: #eef4ff;
+      }
+    `}
+  &:active {
+    transform: scale(1.05);
+  }
+  &:focus {
+    outline: none;
+  }
+  span.bold {
+    font-weight: 700;
   }
 `;
-
 export default SolveButton;
