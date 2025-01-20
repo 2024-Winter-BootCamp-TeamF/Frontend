039d723cf17b7934a8f2932a82b2b4c01b37a150
Hyochan <hyochan02@gmail.com>
2025-01-21 02:31:36 +0900

    design) 업로드페이지, SampleButton 수정

--- src/components/SampleButton.jsx
+++ src/components/SampleButton.jsx
@@ -14,13 +14,13 @@ export default SampleButton;
 const StyledButton = styled.button`
   width: 265px;
   height: 68px;
-  font-size: 16px;
-  font-weight: bold;
+  font-size: 20px;
+  font-weight: 500;
   border-radius: 4px;
   cursor: pointer;
   transition: all 0.3s ease;
   box-sizing: border-box;
-  font-family: "MangoDdobak-B", sans-serif;
+  font-family: "HakgyoansimAllimjangTTF-R";
 
   /* 기본 스타일 */
   ${({ variant }) =>
@@ -34,6 +34,7 @@ const StyledButton = styled.button`
         background-color: white; /* 호버 시 배경색 */
         color: #5c85ff; /* 호버 시 글씨색 */
         border: 2px solid #5c85ff; /* 호버 시 테두리 */
+        font-weight: 700; /* 호버 시 더 두껍게 변경 */
       }
     `}
 
@@ -46,6 +47,7 @@ const StyledButton = styled.button`
 
       &:hover {
         background-color: white;
+        font-weight: 700; /* 호버 시 더 두껍게 변경 */
       }
     `}
 
