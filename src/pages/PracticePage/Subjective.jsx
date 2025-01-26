206bb8a79f32af32de8c7fb04e054ce29103bd21
Hyochan <hyochan02@gmail.com>
2025-01-26 23:50:39 +0900

    Revert "Merge branch 'main' into feat/#101"
    This reverts commit 15c097b303bac99755284695a35941b1ae9469e0, reversing
    changes made to cac1ac08ca82fb5ff8f4f295728aa72b7e3bd7ef.

--- src/pages/PracticePage/Subjective.jsx
+++ src/pages/PracticePage/Subjective.jsx
@@ -6,7 +6,6 @@ import PropTypes from "prop-types";
 const COLORS = {
   PRIMARY: "#5887f4",
   SECONDARY: "#F24822",
-  CORRECT: "#28a745", // 정답 강조 색상
   BORDER: "#E0E0E0",
   BACKGROUND: "#ffffff",
 };
@@ -63,30 +62,13 @@ const Subjective = ({ number, problem, readOnly, onProblemSolved }) => {
     >
       <Title>{`Q.${number}`}</Title>
       <Content>{problem.question}</Content>
-      {isGraded ? (
-        // 채점 후에는 정답과 사용자 답안을 모두 출력
-        <GradingSection>
-          <AnswerWrapper isCorrect>
-            <Label>정답 : </Label>
-            <Text>{problem.correctAnswer || "없음"}</Text>
-          </AnswerWrapper>
-          <AnswerWrapper
-            isCorrect={problem.userAnswer === problem.correctAnswer}
-          >
-            <Label>사용자가 입력한 답 : </Label>
-            <Text>{problem.userAnswer || "입력된 답이 없습니다."}</Text>
-          </AnswerWrapper>
-        </GradingSection>
-      ) : (
-        // 채점 전에는 입력 필드만 표시
-        <Input
-          type="text"
-          placeholder="답을 입력하세요"
-          value={readOnly ? problem.userAnswer : answer}
-          onChange={handleAnswerChange}
-          disabled={readOnly}
-        />
-      )}
+      <Input
+        type="text"
+        placeholder="답을 입력하세요"
+        value={readOnly ? problem.userAnswer : answer}
+        onChange={handleAnswerChange}
+        disabled={readOnly}
+      />
     </SubjectiveContainer>
   );
 };
@@ -137,31 +119,6 @@ const Input = styled.input`
     background-color: #f5f5f5;
     cursor: not-allowed;
   }
-`;
-
-const GradingSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-const AnswerWrapper = styled.div`
-  display: flex;
-  gap: 5px;
-  align-items: center;
-  background-color: ${(props) =>
-    props.isCorrect ? "#eaffea" : "#ffecec"}; // 정답 초록색, 오답 빨간색
-  padding: 10px;
-  border-radius: 4px;
-`;
-
-const Label = styled.span`
-  font-weight: 700;
-  font-size: 18px;
-`;
-
-const Text = styled.span`
-  font-size: 18px;
 `;
 
 export default Subjective;
